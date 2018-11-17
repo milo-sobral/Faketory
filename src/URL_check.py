ad4423cfe263becace62ff49c418145a430738f0
Zoe Lapomme <zoe.lapomme@mail.mcgill.ca>
2018-11-17 16:20:02 -0500

    url recognizer working

--- src/URL_check.py
+++ src/URL_check.py
@@ -1,8 +1,14 @@
 import json, os
+import sys
+sys.path.insert(0,'./create_data')
+import data_labeler as data_labeler
+import os
+from os import listdir
+from os.path import isfile, join
 
-def get_array_URL_checked () :
+def get_array_URL_to_compare () :
   arrayURL = []
-  data = json.loads(open('create_data/sources.json').read())
+  data = json.loads(open('create_data/sources1.json').read())
   for source in data['sources'] :
     source_url = source['url']
     string_to_add = ""
@@ -16,27 +22,61 @@ def get_array_URL_checked () :
     arrayURL.append(string_to_add)
   return arrayURL
 
+
+def get_array_URL_complete ():
+    arrayURL = []
+    data = json.loads(open('create_data/sources1.json').read())
+    for source in data['sources'] :
+        arrayURL.append (source['url'])
+    return arrayURL
+
+
 def check_URL (URL_to_check, arrayURL) :
-    URL_to_remember = "a"
+    i = -1
     for url in arrayURL :
+        i += 1
         num = URL_to_check.find(url)
         if num > -1 :
-            return url
-    if URL_to_remember == "a" :
-        return "no_URL_found"
-    else :
-        return URL_to_remember
+            return i
+    return -1
 
-#def URLscore ()
+
+def URLscore (posURL, arrayURL) :
+    URLname = arrayURL[posURL]
+    name_file_score = ""
+    name_file_score_complete = ""
+    data = json.loads(open('create_data/sources1.json').read())
+    for source in data['sources'] :
+        if source['url'] == URLname :
+            name_file_score = source['id']
+            name_file_score_complete = name_file_score + ".json"
+            break
+    mypath = data_labeler.get_data_file()
+    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+    #getting the dictionary with all the sources and their reliability and bias scores
+    dicReliability = data_labeler.getReliability(files)
+    dicBias = data_labeler.getBias(files)
+
+    #first entry of the score array is the reliability score and the second is the bias score
+    scoresArray = [-1,-1]
+    scoresArray[0] = dicReliability[name_file_score_complete]
+    scoresArray[1] = dicBias [name_file_score_complete]
+
+    return scoresArray
+
+
 
 
 #def main ():
-arrayURL = get_array_URL_checked()
-URL = check_URL ("foxnews", arrayURL)
-score = 0
-if URL == "no_URL_found" :
-    break
+arrayURL_comparison = get_array_URL_to_compare()
+arrayURL_complete = get_array_URL_complete()
+posURL = check_URL ("https://www.foxnews.com/politics/trump-on-cnns-jim-acosta-if-he-misbehaves-well-throw-him-out", arrayURL_comparison)
+score = []
+if posURL == -1 :
+    #return [-1,-1]
+    print (-1)
 else :
-    score = URLscore (URL)
-
-print (URL)
+    score = URLscore (posURL, arrayURL_complete)
+    #return scoreArray
+    print (score)
