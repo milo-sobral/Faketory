9a6943905a1b0d40fac7cbc89ac1864ba68fa6f3
Zoe Lapomme <zoe.lapomme@mail.mcgill.ca>
2018-11-17 12:59:43 -0500

    return file for Greg

---
+++ src/faketory_fun.py
@@ -0,0 +1,31 @@
+# Author : Milo Sobral
+# Main script to check an article from an url
+from extension/webscraper import . as ws
+import extension/webscraper as f1
+
+
+# TODO
+# - Get URL from javascript extension
+# - Request and parse with webscraper
+# - treat
+# - return score
+
+# where to output json file
+
+def get_url() :
+    # Get url from javascript extension
+    return url
+
+def get_json_file(url) :
+    file_evaluating = f1.main(url)
+
+def get_spell_checker_score
+
+
+def main () :
+    get_json_file(get_url())
+    #make model with words
+    #evaluate the json file that we are testing
+    #output score
+
+#how to evaluate score
