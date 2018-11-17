a21b69f374f22db9bc5d3cc6f50c27b0abd6b3f8
Zoe Lapomme <zoe.lapomme@mail.mcgill.ca>
2018-11-17 13:13:18 -0500

    added functions to main file

--- src/faketory_fun.py
+++ src/faketory_fun.py
@@ -1,7 +1,8 @@
 # Author : Milo Sobral
 # Main script to check an article from an url
 from extension/webscraper import . as ws
-import extension/webscraper as f1
+import extension/webscraper as webscraper
+import spellcheck/spellchecker as spellcheck
 
 
 # TODO
@@ -17,15 +18,28 @@ def get_url() :
     return url
 
 def get_json_file(url) :
-    file_evaluating = f1.main(url)
+    file_to_evaluate = webscraper.main(url)
+    return file_to_evaluate
 
-def get_spell_checker_score
+def get_spell_checker_score (file_to_evaluate) :
+    spellcheck_score = spellcheck.main()
+
+def get_reliability_score () :
+
+def get_bias_score () :
+
+def get_URL_recognizer_score () :
+
+def get_final_score (spell_check_score, reliability_score, bias_score, URL_recognizer_score)
 
 
 def main () :
     get_json_file(get_url())
     #make model with words
     #evaluate the json file that we are testing
+    score = get_final_score (get_spell_check_score(file_to_evaluate), get_reliability_score(), get_bias_score(), get_URL_recognizer_score())
     #output score
+    output_score_to_extension(score)
 
 #how to evaluate score
+    #spellchecker
